bc6cd5ae0f1015885cf99bb634983b4fe9855f1c
ubuntu3 <gnextsaloni100@gmail.com>
2019-03-13 03:26:30 +0000

    drawables deleted , new drawables added

---
+++ app/src/main/java/com/aniket/tmvoter/UseAsActivity.java
@@ -0,0 +1,13 @@
+package com.aniket.tmvoter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class UseAsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_use_as_activity);
+    }
+}
