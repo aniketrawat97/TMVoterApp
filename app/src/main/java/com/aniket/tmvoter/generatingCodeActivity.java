536cc4443ce60e600ca22eb66697c60225991bc4
ubuntu3 <gnextsaloni100@gmail.com>
2019-03-13 02:12:22 +0000

    drawables modified,activities modified

---
+++ app/src/main/java/com/aniket/tmvoter/generatingCodeActivity.java
@@ -0,0 +1,13 @@
+package com.aniket.tmvoter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class generatingCodeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_generating_code);
+    }
+}
